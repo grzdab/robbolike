3a9574886c5882b2a1bc7bb5e4594f7665822ce7
Michał Bednarczyk <michal.bednarczyk93@gmail.com>
2022-03-24 10:45:30 +0100

    Add inventory to Player

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,8 +1,10 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
+import com.codecool.dungeoncrawl.Inventory;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Player extends Actor {
+    Inventory inventory;
     public Player(Cell cell) {
         super(cell);
 //        super(cell, monsters);
